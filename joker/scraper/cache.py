86bad0e0bae26d0d7cb03cc3ca0c900774662731
Hailong <lendoli@163.com>
2020-01-17 22:47:17 +0800

    ScatteredDiskCache

--- joker/scraper/cache.py
+++ joker/scraper/cache.py
@@ -16,8 +16,11 @@ class DiskCache(object):
             raise ValueError('not a dir: ' + dirpath)
         self.dirpath = dirpath
 
+    def get_path(self, key):
+        return os.path.join(self.dirpath, key)
+
     def load(self, key):
-        path = os.path.join(self.dirpath, key)
+        path = self.get_path(key)
         if not os.path.exists(path):
             return
         logger.debug('use cached: ' + path)
@@ -32,12 +35,26 @@ class DiskCache(object):
             traceback.print_exc()
 
     def save(self, key, content):
-        path = os.path.join(self.dirpath, key)
+        path = self.get_path(key)
+        logger.debug('save to cache: ' + path)
         hb = hashlib.md5(content).digest()
         with gzip.open(path, 'wb') as fout:
             fout.write(content)
             fout.write(hb)
         return content
+
+
+class ScatteredDiskCache(DiskCache):
+    def __init__(self, dirpath, prefixlen=4):
+        DiskCache.__init__(self, dirpath)
+        self.prefixlen = prefixlen
+
+    def get_path(self, key):
+        prefix = key[:self.prefixlen]
+        pdirpath = os.path.join(self.dirpath, prefix)
+        if not os.path.isdir(pdirpath):
+            os.makedirs(pdirpath, exist_ok=True)
+        return os.path.join(pdirpath, key)
 
 
 SimpleDiskCache = DiskCache
